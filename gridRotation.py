8e08298b525f6485521632728e041c627b085321
Kunal Verma <vkunal1996@users.noreply.github.com>
2018-04-22 18:02:21 +0530

    Update gridRotation.py

--- gridRotation.py
+++ gridRotation.py
@@ -10,7 +10,7 @@ grid=[['.','.','.','.','.','.'],
       ['.','.','.','.','.','.'],
      ]
 newgrid=copy.deepcopy(grid)
-for i in range(0,len(newgrid)):
+for i in range(0,len(newgrid)-3):
     for j in range(0,len(newgrid[i])+3):
         print(newgrid[j][i],end=' ')
     print()
